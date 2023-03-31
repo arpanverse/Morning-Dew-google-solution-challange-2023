ded1cf1d03b62cb8f51140bccf17aa5070c2631c
Arpan Das <arpandas@Arpans-MacBook-Air.local>
2023-04-01 02:12:25 +0530

    removed custom stories

--- lib/Globals/global.dart
+++ lib/Globals/global.dart
@@ -3,25 +3,9 @@ List category = [
 ];
 var category_select;
 
-List post_story = [
+List post_story = [];
 
-"She clutched the faded photograph to her chest, tears streaming down her face. It had been years since she'd seen him, but the memories flooded back like it was yesterday. The laughter, the late nights, the whispered promises. She couldn't believe it was all over. With a heavy heart, she placed the photo back in its frame and turned to leave. But as she reached for the doorknob, it turned and he stood before her, a bouquet of her favorite flowers in his hand. I never stopped loving you, he said, and she knew in that moment that they were meant to be together forever."
-
-,"She walked down the dusty road, her heart heavy with memories of the past. Suddenly, a familiar tune caught her attention. She looked up to see an old man playing a guitar, his eyes closed in concentration. As he finished the last note, he opened his eyes and smiled at her. In that moment, she felt a warmth in her heart and knew that she would carry this memory with her forever."
-
-,"As I walked into the old attic, memories flooded back to me. It was here that I used to play with my late grandfather when I was a child. Looking around, I saw his old chair, covered in dust and cobwebs. Sitting in it, I felt his presence once more. I closed my eyes, taking in the familiar scent of his pipe tobacco, and smiled."
-
-];
-
-List post_task = [
-
-"She clutched the faded photograph to her chest, tears streaming down her face. It had been years since she'd seen him, but the memories flooded back like it was yesterday. The laughter, the late nights, the whispered promises. She couldn't believe it was all over. With a heavy heart, she placed the photo back in its frame and turned to leave. But as she reached for the doorknob, it turned and he stood before her, a bouquet of her favorite flowers in his hand. I never stopped loving you, he said, and she knew in that moment that they were meant to be together forever."
-
-,"She walked down the dusty road, her heart heavy with memories of the past. Suddenly, a familiar tune caught her attention. She looked up to see an old man playing a guitar, his eyes closed in concentration. As he finished the last note, he opened his eyes and smiled at her. In that moment, she felt a warmth in her heart and knew that she would carry this memory with her forever."
-
-,"As I walked into the old attic, memories flooded back to me. It was here that I used to play with my late grandfather when I was a child. Looking around, I saw his old chair, covered in dust and cobwebs. Sitting in it, I felt his presence once more. I closed my eyes, taking in the familiar scent of his pipe tobacco, and smiled."
-
-];
+List post_task = [];
 
 
 List acct_story = [];
